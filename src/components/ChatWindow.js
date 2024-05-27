cdd825a74a4eb293c9095db5902c08439462c83f
Bibhushan <bibhus47@in.ibm.com>
2024-05-27 20:08:05 +0530

    Applied styles in Chat window

--- src/components/ChatWindow.js
+++ src/components/ChatWindow.js
@@ -1,3 +1,4 @@
+import { Grid } from '@mui/material';
 import logo from '../assets/botAI.png';
 import QuestionCard from './QuestionCard';
 
@@ -19,13 +20,13 @@ export default function ChatWindow(){
                 <h2 style={{color:'black'}}>How Can I Help You Today?</h2>
                 <img src={logo} alt='Bot AI' style={{height:69, width:'auto'}} />
             </div>
-            <div style={{display:'flex', flexWrap:'wrap'}}>
-                {topChats.map((chat)=><QuestionCard question={chat.question} answer={chat.answer}/>)}
-            </div>
-            <div style={{display:'flex', justifyContent:'center', alignItems:'center', margin:'1rem'}}>
+            <Grid container sx={{display:'flex', flexWrap:'wrap'}}>
+                {topChats.map((chat, index)=><QuestionCard question={chat.question} answer={chat.answer} key={'key-card-'+index}/>)}
+            </Grid>
+            <div style={{display:'flex', justifyContent:'center', alignItems:'center', margin:'1rem 0rem'}}>
                 <input style={{padding:'0.5rem', borderRadius:'5px', border:'1px solid #00000073', width:'100%', margin:'0.5rem'}} type='text'/>
-                <button className='bot-button' style={{fontSize:20, fontWeight:400, margin:'0.25rem'}}>Ask</button>
-                <button className='bot-button' style={{fontSize:20, fontWeight:400, margin: '0.25rem'}}>Save</button>
+                <button className='bot-button chat-button' >Ask</button>
+                <button className='bot-button chat-button' >Save</button>
             </div>
         </div>
     )
--- src/components/QuestionCard.js
+++ src/components/QuestionCard.js
@@ -1,14 +1,16 @@
-import { Card, CardContent, CardHeader } from "@mui/material";
+import { Card, CardContent, CardHeader, Grid } from "@mui/material";
 
 export default function QuestionCard({question, answer}){
 
     return(
-        <Card sx={{minWidth:500, padding:'0.5rem', margin:'1rem 0rem 0.5rem 1rem', borderRadius:'5px'}}>
-            <CardContent sx={{textAlign:'left'}}>
-                <h3>{question}</h3>
-                <p>{answer}</p>
-            </CardContent>
-        </Card>
+        <Grid item md={6}>
+            <Card sx={{minWidth:300, padding:'0.5rem', margin:'1rem 0rem 0.5rem 1rem', borderRadius:'5px'}}>
+                <CardContent sx={{textAlign:'left'}}>
+                    <h3>{question}</h3>
+                    <p>{answer}</p>
+                </CardContent>
+            </Card>
+        </Grid>
     )
 }
 
